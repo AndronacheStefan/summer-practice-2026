e1d6d5136c3cae4b91b3bf3c35c6b147552a9602
RaduTek <me@radutek.uk>
2026-07-07 14:14:05 +0300

    Backend: Update device schema

--- backend/Application/database/models.py
+++ backend/Application/database/models.py
@@ -11,11 +11,24 @@ class User(Document):
 
 class Device(Document):
     deviceName = StringField(required=True, unique=True)
+    deviceSlNo = StringField(required=False)
+    deviceType = StringField(required=False)
+    hwType = StringField(required=False)
     group = StringField(required=True)
-    powerOnTime = StringField(required=True)  # format: "HH:MM"
-    powerOffTime = StringField(required=True)  # format: "HH:MM"
-    count = IntField(required=True, default=1)  # câte dispozitive de acest tip
-    consumptionPerHour = FloatField(required=True)  # kWh per hour
+    site = StringField(required=False)
+    owner = StringField(required=False)
+    connectivityType = StringField(required=False)
+    ip = StringField(required=False)
+    port = StringField(required=False)
+    loginUser = StringField(required=False)
+    password = StringField(required=False)
+    readCommunity = StringField(required=False)
+    writeCommunity = StringField(required=False)
+    powerOnTime = StringField(required=False)  # format: "HH:MM"
+    powerOffTime = StringField(required=False)  # format: "HH:MM"
+    count = IntField(required=False, default=1)  # câte dispozitive de acest tip
+    consumptionPerHour = FloatField(required=False)  # kWh per hour
+    
 
 class DailySaving(EmbeddedDocument):
     subId = ObjectIdField(required=True, default=lambda: ObjectId())
